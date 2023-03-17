e66581276ce4fa8c0a6abd74b1076d7e6d89aa6e
rafaelDev0ps <devops@rafaelmattos.com>
2023-03-17 09:25:05 -0300

    Setup New Relic package in code

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 import json
-        
+import newrelic.agent
+
+newrelic.agent.initialize()
+
+@newrelic.agent.initialize()
 def lambda_handler(event, context):
     json_region = os.environ['AWS_REGION']
     return {
