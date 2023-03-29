bf7f5d1d45361e3d33cfecab64aa21676d98e69e
rafaelDev0ps <devops@rafaelmattos.com>
2023-03-29 16:16:15 -0300

    Remove NewRelic python lib and import NewRelic Lambda layer

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -17,15 +17,21 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = "python-lambda-newrelic"
   role          = data.aws_iam_role.lambda-role.arn
   handler       = "app.lambda_handler"
-
+  layers = [
+    "arn:aws:lambda:us-east-1:451483290750:layer:NewRelicPython39:38",
+  ]
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.9"
 
   environment {
     variables = {
-      tool = "terraform"
-      app  = "lambda"
+      tool                                   = "terraform"
+      app                                    = "lambda"
+      NEW_RELIC_ACCOUNT_ID                   = "3830625"
+      NEW_RELIC_EXTENSION_SEND_FUNCTION_LOGS = "false"
+      NEW_RELIC_LAMBDA_EXTENSION_ENABLED     = "true"
+      NEW_RELIC_LAMBDA_HANDLER               = "app.lambda_handler"
     }
   }
 }
